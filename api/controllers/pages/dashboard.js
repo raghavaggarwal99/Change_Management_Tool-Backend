351a7ff82e697cbe572b32abaf95219757cc4c9b
raghavaggarwal99 <ragha170103048@iitg.ac.in>
2020-07-16 15:11:30 +0530

    dashboard

--- api/controllers/pages/dashboard.js
+++ api/controllers/pages/dashboard.js
@@ -2,6 +2,17 @@ module.exports = {
 
 
     friendlyName: 'This is the most difficult part, here i will have all functionalty when all stuff happens',
+
+
+    inputs: {
+
+      PageId: {
+        description: 'The page in pagination',
+        type: 'number',
+        required: true
+      },
+
+    },
   
     exits: {
   
@@ -16,50 +27,96 @@ module.exports = {
     },
   
   
-    fn: async function () {
-  
-      //Current logged in user id i am taking it "2" Sahil basically
+    fn: async function (inputs, exits) {
 
-      var requestrecords = await Request.find({
+    let PerPage= 5;
+
+    let PageId= inputs.PageId;
+    let Start=(PageId-1)*PerPage
+
+      var RequestRecords = await Request.find({
         isDeleted: 0,
       });
 
-      var finalusers = [];
-      
-      for (const requestrecord of requestrecords) {
-        var status= requestrecord.status;
+      //It contains Request Info and request Ids to show
+      var FinalRequests = {};
 
-          var statusrecords= await Statusoutlinemapping.find({
-              initialstatus: status,
+      //It contains those user for which we will show approve and Decline button
+      let FinalUsers= {}
+
+       //It contains User details, who have created this request
+      let UserDetails= {}
+
+      
+      for (const RequestRecord of RequestRecords) {
+        var Status= RequestRecord.status;
+
+        FinalUsers[RequestRecord.id] = new Array();
+
+          var StatusRecords= await Statusoutlinemapping.find({
+              initialstatus: Status,
           });
 
-          for (const statusrecord of statusrecords){
+          for (const StatusRecord of StatusRecords){
 
-              var permissionstatus= statusrecord.finalstatus;
-              var shownusers= await Permission.find({
-                    Feature : permissionstatus,
-                    userId: 3,
+              var PermissionStatus= StatusRecord.finalstatus;
+              
+              var ShownUsers= await Permission.find({
+                    Feature : PermissionStatus,
+                    userId: this.req.userId,
                 })
                 var obj ={}
-                var check= await sails.helpers.checkparent.with({userid: requestrecord.userId, currentId: 3})
+                var check= await sails.helpers.checkparent.with({userid: RequestRecord.userId, currentId: this.req.userId})
 
-                if (Array.isArray(shownusers) && shownusers.length && check==1){
-                  obj["requestId"]=requestrecord.id;
-                  obj["requestinfo"]=await Request.find({id:requestrecord.id});
-                  obj["userinfo"]=shownusers;
+                if (Array.isArray(ShownUsers) && ShownUsers.length && check==1){
+
+                  obj["requestId"]=RequestRecord.id;
+                  obj["requestinfo"]=await Request.find({id:RequestRecord.id});
+
+                  FinalRequests[RequestRecord.id] = new Array();
+                 
+
+                    if(Object.keys(obj).length !== 0){
+                     
+                      FinalRequests[RequestRecord.id]= (obj)
+                      FinalUsers[RequestRecord.id].push(ShownUsers);
+                      UserDetails[RequestRecord.userId]= await User.find({id:RequestRecord.userId});
+
+                    }
                   
-                  finalusers.push(obj)
                 }
                 
           }
           
       }
 
-      //check if the current user is his direct parent or not
-      
-      return this.res.json(200, {users: finalusers});
+      console.log(Start);
+
+      let totalPages= await getPages(FinalRequests, PerPage);
+
+      //This is returning the records accrding to the pagination
+      FinalRequests= Object.entries(FinalRequests).slice(Start,Start+PerPage).map(entry => entry[1]);
+
+      return this.res.json(200, {users: FinalUsers, requests: FinalRequests, userdetails: UserDetails, totalPages: totalPages});
 
     }
   
   };
   
+
+async function getPages(FinalRequests, PerPage){
+
+  let totalPages=0;
+
+  if( Object.keys(FinalRequests).length !== 0){
+
+    let NumberOfRecords= Object.keys(FinalRequests).length 
+
+    totalPages= Math.ceil(NumberOfRecords/PerPage);
+
+  }
+
+  return totalPages
+
+
+}
